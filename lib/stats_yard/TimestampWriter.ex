7173bcd1b45d5ce5f2d8a051a43ac7b6090e47de
Cody Boggs <cjboggs@acm.org>
2016-03-27 21:42:33 -0600

    add initial increment of TimestampWriter GenServer

---
+++ lib/stats_yard/TimestampWriter.ex
@@ -0,0 +1,28 @@
+defmodule StatsYard.TimestampWriter do
+  use GenServer
+
+  ###
+  ### Public API
+  ###
+
+  def write_timestamp(filename, timestamp) do
+    GenServer.cast(__MODULE__, {:write_timestamp, filename, timestamp})
+  end
+
+  def start_link() do
+    {:ok, _pid} = GenServer.start_link(__MODULE__, 0, name: __MODULE__)
+  end
+
+  ###
+  ### GenServer callbacks
+  ###
+
+  def init(state) do
+    {:ok, state}
+  end
+
+  def handle_cast({:write_timestamp, filename, timestamp}, state) do
+    :ok = File.write(filename, "#{timestamp}")
+    {:noreply, state}
+  end
+end
