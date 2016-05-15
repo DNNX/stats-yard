751bc4c3cbb5a49e4afb85828c044202526a9592
Viktar Basharymau <viktar.basharymau@gmail.com>
2016-05-15 13:31:48 +0300

    Fix unused variable warnings

--- test/stats_yard/data_point_test.exs
+++ test/stats_yard/data_point_test.exs
@@ -15,7 +15,7 @@ defmodule StatsYard.DataPointTest do
   test "can validate valid data points" do
     for valid_data_point <- @valid_data_points do
       assert capture_log([level: :debug], fn ->
-        assert {:ok, valid_data_point} = StatsYard.DataPoint.validate(valid_data_point)
+        assert {:ok, ^valid_data_point} = StatsYard.DataPoint.validate(valid_data_point)
       end) =~ "valid stat: #{inspect(valid_data_point)}"
     end
   end
@@ -23,7 +23,7 @@ defmodule StatsYard.DataPointTest do
   test "can invalidate invalid data points" do
     for invalid_data_point <- @invalid_data_points do
       assert capture_log([level: :info], fn ->
-        assert {:invalid, invalid_data_point} = StatsYard.DataPoint.validate(invalid_data_point)
+        assert {:invalid, ^invalid_data_point} = StatsYard.DataPoint.validate(invalid_data_point)
       end) =~ "invalid stat: #{inspect(invalid_data_point)}"
     end
   end
